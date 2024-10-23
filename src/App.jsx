bd2150ba9d25358a2863637f268267f136077273
Leon Chinn <leonchinn1993@gmail.com>
2024-10-23 15:54:45 +1100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,19 @@
+// components
+import Navbar from "./Navbar";
+import CartContainer from "./CartContainer";
+import { useGlobalContext } from "./context";
+
+function App() {
+  const { loading } = useGlobalContext();
+  if (loading) {
+    return <div className="loading"></div>;
+  }
+  return (
+    <main>
+      <Navbar />
+      <CartContainer />
+    </main>
+  );
+}
+
+export default App;
